0d28e944e25a0d2ad52e505d01d2624bbbd5b681
ho3eintahan <hoseintahan84@gmail.com>
2024-11-20 19:30:26 +0330

    Updated UI DefineKala form

--- Forms/DefineKala.Designer.cs
+++ Forms/DefineKala.Designer.cs
@@ -39,11 +39,21 @@
             label2 = new Label();
             label1 = new Label();
             textBox1 = new TextBox();
+            btnSave = new Button();
+            button1 = new Button();
+            label7 = new Label();
+            textBox5 = new TextBox();
+            label8 = new Label();
             groupBox1.SuspendLayout();
             SuspendLayout();
             // 
             // groupBox1
             // 
+            groupBox1.Controls.Add(label8);
+            groupBox1.Controls.Add(label7);
+            groupBox1.Controls.Add(textBox5);
+            groupBox1.Controls.Add(button1);
+            groupBox1.Controls.Add(btnSave);
             groupBox1.Controls.Add(label6);
             groupBox1.Controls.Add(textBox4);
             groupBox1.Controls.Add(label5);
@@ -54,9 +64,10 @@
             groupBox1.Controls.Add(label2);
             groupBox1.Controls.Add(label1);
             groupBox1.Controls.Add(textBox1);
-            groupBox1.Location = new Point(12, 12);
+            groupBox1.Dock = DockStyle.Fill;
+            groupBox1.Location = new Point(0, 0);
             groupBox1.Name = "groupBox1";
-            groupBox1.Size = new Size(521, 298);
+            groupBox1.Size = new Size(320, 227);
             groupBox1.TabIndex = 0;
             groupBox1.TabStop = false;
             groupBox1.Text = "تعریف کالا";
@@ -65,7 +76,7 @@
             // label6
             // 
             label6.AutoSize = true;
-            label6.Location = new Point(472, 100);
+            label6.Location = new Point(112, 129);
             label6.Name = "label6";
             label6.RightToLeft = RightToLeft.Yes;
             label6.Size = new Size(46, 15);
@@ -74,7 +85,7 @@
             // 
             // textBox4
             // 
-            textBox4.Location = new Point(362, 97);
+            textBox4.Location = new Point(8, 124);
             textBox4.Name = "textBox4";
             textBox4.PlaceholderText = "درصد تخفیف";
             textBox4.RightToLeft = RightToLeft.Yes;
@@ -84,26 +95,27 @@
             // label5
             // 
             label5.AutoSize = true;
-            label5.Location = new Point(116, 57);
+            label5.Location = new Point(112, 99);
             label5.Name = "label5";
             label5.RightToLeft = RightToLeft.Yes;
-            label5.Size = new Size(39, 15);
+            label5.Size = new Size(54, 15);
             label5.TabIndex = 7;
-            label5.Text = "تعداد :";
+            label5.Text = "موجودی :";
             // 
             // textBox3
             // 
-            textBox3.Location = new Point(10, 54);
+            textBox3.Location = new Point(8, 94);
             textBox3.Name = "textBox3";
-            textBox3.PlaceholderText = "تعداد کالا";
+            textBox3.PlaceholderText = "موجودی کالا";
             textBox3.RightToLeft = RightToLeft.Yes;
             textBox3.Size = new Size(100, 23);
             textBox3.TabIndex = 6;
             // 
             // label4
             // 
+            label4.Anchor = AnchorStyles.Top | AnchorStyles.Right;
             label4.AutoSize = true;
-            label4.Location = new Point(468, 57);
+            label4.Location = new Point(271, 98);
             label4.Name = "label4";
             label4.RightToLeft = RightToLeft.Yes;
             label4.Size = new Size(46, 15);
@@ -112,8 +124,9 @@
             // 
             // label3
             // 
+            label3.Anchor = AnchorStyles.Top | AnchorStyles.Right;
             label3.AutoSize = true;
-            label3.Location = new Point(290, 57);
+            label3.Location = new Point(271, 127);
             label3.Name = "label3";
             label3.RightToLeft = RightToLeft.Yes;
             label3.Size = new Size(41, 15);
@@ -122,7 +135,8 @@
             // 
             // textBox2
             // 
-            textBox2.Location = new Point(184, 54);
+            textBox2.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            textBox2.Location = new Point(167, 123);
             textBox2.Name = "textBox2";
             textBox2.PlaceholderText = "قیمت کالا";
             textBox2.RightToLeft = RightToLeft.Yes;
@@ -131,8 +145,9 @@
             // 
             // label2
             // 
+            label2.Anchor = AnchorStyles.Top | AnchorStyles.Right;
             label2.AutoSize = true;
-            label2.Location = new Point(415, 19);
+            label2.Location = new Point(209, 19);
             label2.Name = "label2";
             label2.Size = new Size(65, 15);
             label2.TabIndex = 2;
@@ -140,8 +155,9 @@
             // 
             // label1
             // 
+            label1.Anchor = AnchorStyles.Top | AnchorStyles.Right;
             label1.AutoSize = true;
-            label1.Location = new Point(478, 19);
+            label1.Location = new Point(273, 19);
             label1.Name = "label1";
             label1.RightToLeft = RightToLeft.Yes;
             label1.Size = new Size(37, 15);
@@ -150,8 +166,9 @@
             // 
             // textBox1
             // 
+            textBox1.Anchor = AnchorStyles.Top | AnchorStyles.Right;
             textBox1.BackColor = SystemColors.Window;
-            textBox1.Location = new Point(362, 54);
+            textBox1.Location = new Point(167, 94);
             textBox1.Name = "textBox1";
             textBox1.PlaceholderText = "نام کالا";
             textBox1.RightToLeft = RightToLeft.Yes;
@@ -159,11 +176,74 @@
             textBox1.TabIndex = 0;
             textBox1.TextChanged += textBox1_TextChanged;
             // 
+            // btnSave
+            // 
+            btnSave.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+            btnSave.BackColor = Color.FromArgb(0, 192, 0);
+            btnSave.BackgroundImageLayout = ImageLayout.Zoom;
+            btnSave.Font = new Font("Segoe UI", 15F, FontStyle.Bold);
+            btnSave.ForeColor = Color.White;
+            btnSave.Location = new Point(202, 176);
+            btnSave.Name = "btnSave";
+            btnSave.Size = new Size(110, 39);
+            btnSave.TabIndex = 27;
+            btnSave.Text = "ثبت";
+            btnSave.UseVisualStyleBackColor = false;
+            // 
+            // button1
+            // 
+            button1.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
+            button1.BackColor = Color.Red;
+            button1.BackgroundImageLayout = ImageLayout.Zoom;
+            button1.Font = new Font("Segoe UI", 15F, FontStyle.Bold);
+            button1.ForeColor = Color.White;
+            button1.Location = new Point(8, 176);
+            button1.Name = "button1";
+            button1.Size = new Size(110, 39);
+            button1.TabIndex = 28;
+            button1.Text = "انصراف";
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // label7
+            // 
+            label7.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            label7.AutoSize = true;
+            label7.Location = new Point(271, 51);
+            label7.Name = "label7";
+            label7.RightToLeft = RightToLeft.Yes;
+            label7.Size = new Size(44, 15);
+            label7.TabIndex = 30;
+            label7.Text = "کد کالا :";
+            // 
+            // textBox5
+            // 
+            textBox5.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            textBox5.BackColor = SystemColors.Window;
+            textBox5.Location = new Point(167, 47);
+            textBox5.Name = "textBox5";
+            textBox5.PlaceholderText = "کدکالا";
+            textBox5.RightToLeft = RightToLeft.Yes;
+            textBox5.Size = new Size(100, 23);
+            textBox5.TabIndex = 29;
+            // 
+            // label8
+            // 
+            label8.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            label8.AutoSize = true;
+            label8.Font = new Font("Segoe UI", 6F, FontStyle.Bold);
+            label8.ForeColor = Color.Red;
+            label8.Location = new Point(12, 53);
+            label8.Name = "label8";
+            label8.RightToLeft = RightToLeft.Yes;
+            label8.Size = new Size(149, 11);
+            label8.TabIndex = 31;
+            label8.Text = "توجه : درصورت وجود بارکد ,بارکد را وارد کنید .";
+            // 
             // DefineKala
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(546, 322);
+            ClientSize = new Size(320, 227);
             Controls.Add(groupBox1);
             FormBorderStyle = FormBorderStyle.FixedSingle;
             MaximizeBox = false;
@@ -187,5 +267,10 @@
         private TextBox textBox3;
         private Label label6;
         private TextBox textBox4;
+        private Button btnSave;
+        private Label label8;
+        private Label label7;
+        private TextBox textBox5;
+        private Button button1;
     }
 }
